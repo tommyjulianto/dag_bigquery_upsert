98951af6659772ba70b2899dce2b5659b48d2793
tommy <tommy.julianto@koinworks.com>
2019-10-22 11:27:31 +0700

    anoher commit

--- test_bigquery_upsert.py
+++ test_bigquery_upsert.py
@@ -12,7 +12,6 @@ from google.cloud import storage
 import sys
 
 dt = datetime.strptime(sys.argv[2], "%Y-%m-%d %H:%M:%S.%f")
-                                                                )
 sc = SparkContext()
 
 ## read_files & read dataframe
@@ -62,36 +61,4 @@ save_schema(new_df, 'upsert_data')
 
 os.system('gsutil cp ./schema_upsert_data.json gs://spectre-dump-staging/schemas/')
 os.remove('./schema_upsert_data.json')
-
-# for t in range(len(table_list)):
-# 	schema_raw = json.loads(table_list[t][0].schema.json())
-
-# 	print(schema_raw)
-
-# 	for s in schema_raw["fields"]:
-# 		s.pop("metadata")
-# 		s.pop("nullable")
-
-# 	schema_json = json.dumps(schema_raw["fields"])
-# 	print(schema_json)
-# 	file_json = open('./schema_{}.json'.format(table_list[t][1]),'w') 
-# 	file_json.write(schema_json)
-
-# 	# """Uploads a file to the bucket."""
-# 	storage_client = storage.Client()
-# 	bucket = storage_client.get_bucket('spectre-dump-staging')
-# 	blob = bucket.blob('schemas/schema_{}.json'.format(table_list[t][1]))
-# 	blob.upload_from_filename('./schema_{}.json'.format(table_list[t][1]))
-
-# 	os.remove('./schema_{}.json'.format(table_list[t][1]))
-
-
-# schema = withdrawals.schema
-# schema_json = schema.json()
-# schema_json = json.loads(schema_json)
-# new = schema_json['fields']
-
-# for a in new:
-#    a.pop('metadata', None)
-#    a.pop('nullable', None)
 
